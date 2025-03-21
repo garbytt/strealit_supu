6af464895a0ab9f063e084097920c67c82848961
admin <a5816049@aoyama.jp>
2025-03-21 14:55:52 +0900

    fixed path

--- demo_app/pages/page_3.py
+++ demo_app/pages/page_3.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-df = pd.read_csv("./data/random.csv")
+st.write("Current Working Directory:", os.getcwd())
+st.write("Files in current directory:", os.listdir("."))
+st.write("Files in 'data' directory:", os.listdir("data") if os.path.exists("data") else "No 'data' directory found")
+
+data_dir = os.path.join(os.getcwd(), "demo_app", "data")
+csv_path = os.path.join(data_dir, "random.csv")
+df = pd.read_csv(csv_path)
 st.dataframe(df)
 st.table(df)
 st.line_chart(df)
