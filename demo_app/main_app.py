4d5d7e603efeaac6d721c8ac5b205954b23cbafe
admin <a5816049@aoyama.jp>
2025-03-21 14:22:06 +0900

    fixed path

--- demo_app/main_app.py
+++ demo_app/main_app.py
@@ -9,6 +9,9 @@ st.write("Current Working Directory:", os.getcwd())
 st.write("Files in current directory:", os.listdir("."))
 st.write("Files in 'data' directory:", os.listdir("data") if os.path.exists("data") else "No 'data' directory found")
 
+data_dir = os.path.join(os.getcwd(), "data")
+image_path = os.path.join(data_dir, "EVdrGOqU0AUJ5HW.jpg")
+st.write(image_path)
 
 st.title("demo app")
 st.caption("this is a demo app")
