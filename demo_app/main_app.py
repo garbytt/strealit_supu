b5b22281dc07538f3d899ee972119180260d992a
admin <a5816049@aoyama.jp>
2025-03-21 14:17:01 +0900

    fixed path

--- demo_app/main_app.py
+++ demo_app/main_app.py
@@ -1,17 +1,9 @@
 import streamlit as st
 from PIL import Image
-import os
-
-# スクリプトの絶対パスを取得
-current_file_path = os.path.abspath(__file__)
-
-# スクリプトがあるディレクトリのパスを取得
-current_directory = os.path.dirname(current_file_path)
-
-# 目的のファイルへの相対パスを作成
-target_file_path = os.path.join(current_directory, 'EVdrGOqU0AUJ5HW.jpg')
+import pandas as pd
+import matplotlib.pyplot as plt
 
 st.title("demo app")
 st.caption("this is a demo app")
-image = Image.open(target_file_path)
+image = Image.open("./data/EVdrGOqU0AUJ5HW.jpg")
 st.image(image, caption="this is a image", width=200)
--- demo_app/pages/page_3.py
+++ demo_app/pages/page_3.py
@@ -2,16 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# スクリプトの絶対パスを取得
-current_file_path = os.path.abspath(__file__)
-
-# スクリプトがあるディレクトリのパスを取得
-current_directory = os.path.dirname(current_file_path)
-
-# 目的のファイルへの相対パスを作成
-target_file_path = os.path.join(current_directory, 'random.csv')
-
-df = pd.read_csv(target_file_path)
+df = pd.read_csv("./data/random.csv")
 st.dataframe(df)
 st.table(df)
 st.line_chart(df)
