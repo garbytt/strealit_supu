631e020cd4afa498cae020c4a4eb59f993484e93
admin <a5816049@aoyama.jp>
2025-03-21 14:20:39 +0900

    fixed path

--- demo_app/main_app.py
+++ demo_app/main_app.py
@@ -3,6 +3,13 @@ from PIL import Image
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import os
+
+st.write("Current Working Directory:", os.getcwd())
+st.write("Files in current directory:", os.listdir("."))
+st.write("Files in 'data' directory:", os.listdir("data") if os.path.exists("data") else "No 'data' directory found")
+
+
 st.title("demo app")
 st.caption("this is a demo app")
 image = Image.open("./data/EVdrGOqU0AUJ5HW.jpg")
